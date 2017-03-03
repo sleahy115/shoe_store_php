b6fb0a1f21998b650e766bd15553ec8290733ccb
Sarah Leahy <sleahy115@gmail.com>
2017-03-03 09:23:50 -0800

    create twig to enter, list and delete all stores

--- app/app.php
+++ app/app.php
@@ -19,5 +19,20 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
-        });
+    ));
+
+    $app->get("/", function() use ($app) {
+    return $app['twig']->render('index.html.twig',array('stores'=>Store::getAll()));
+    });
+    $app->post("/", function() use ($app) {
+        $store_name = $_POST['store_name'];
+        $new_store = new Store($store_name);
+        $new_store->save();
+    return $app['twig']->render('index.html.twig', array('stores'=>Store::getAll()));
+    });
+    $app->get("/delete_all_stores", function() use ($app) {
+        Store::deleteAll();
+    return $app['twig']->render('index.html.twig', array('stores'=>Store::getAll()));
+    });
+
 return $app;
